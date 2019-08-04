e401ae67eb95e4b923a96daf1d1d581f65624bd2
Alex Powell <alexjohnpowell93@gmail.com>
2019-08-04 15:53:03 +0100

    Initial setup

---
+++ SmartFrame.py
@@ -0,0 +1,29 @@
+# !/bin/python
+""" This module defines the main inerconnecting framework
+ntrol.
+
+It should deal with initilizing moudules, routing messages.
+"""
+
+from __future__ import barry_as_FLUFL
+
+__all__ = []
+__version__ = '0.1'
+__author__ = 'Alex Powell, Freddie Sherrat'
+
+
+class SmartFrame():
+	"""
+
+	"""
+	def __init__():
+		pass
+
+	"""
+
+	"""
+	def SmartFrame():
+		pass
+
+if __name__=='__main__':
+	sf = SmartFrame()
