4647d75d8ded2b20dabe865a7296ffe396567358
Alex Powell <alexjohnpowell93@gmail.com>
2019-08-07 21:54:08 +0100

    Initial pixhawk class setup

--- SmartFrame.py
+++ SmartFrame.py
@@ -5,25 +5,29 @@ ntrol.
 It should deal with initilizing moudules, routing messages.
 """
 
-from __future__ import barry_as_FLUFL
-
 __all__ = []
 __version__ = '0.1'
 __author__ = 'Alex Powell, Freddie Sherrat'
 
+import utilities.argparser as ap
+import modules.pixhawk as px
 
+"""
+Deals with message routing.
+Setup should allow for items to be configured at runtime
+"""
 class SmartFrame():
 	"""
 
 	"""
-	def __init__():
-		pass
-
-	"""
-
-	"""
-	def SmartFrame():
-		pass
+	def __init__( self, pixhawk ):
+		self.pixhawk = pixhawk
 
 if __name__=='__main__':
-	sf = SmartFrame()
+	# setup arguments
+	args = ap.GetParser().parse_args()
+	# Get pixhawk
+	pix = px.pixhawk( args.pix[0], args.pix[1] )
+	
+	# Startup frame
+	sf = SmartFrame( pix )
---
+++ modules/pixhawk.py
@@ -0,0 +1,4 @@
+class pixhawk():
+	def __init__(self, port, baud):
+		self.port = port
+		self.baud = baud
