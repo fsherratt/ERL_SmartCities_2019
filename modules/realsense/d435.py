00f2ebcf99b0cc592c34c2f154f64ee874d3240d
Freddie Sherratt <fsherratt@gmail.com>
2019-08-10 14:34:29 +0100

    Added basic connection code from objectMap.py

--- modules/realsense/d435.py
+++ modules/realsense/d435.py
@@ -2,6 +2,8 @@ import pyrealsense2 as rs
 import traceback
 import sys
 
+import numpy as np
+
 class unexpectedDisconnect( Exception):
     # Camera unexpectably disconnected
     pass
@@ -20,10 +22,44 @@ class rs_t265:
         self.closeConnection()
 
     def openConnection(self):
-        pass
+        self.pipe = rs.pipeline()
+
+        self.width = 640
+        self.height = 480
+        self.fps = 6
+
+        self.cfg = rs.config()
+        self.cfg.enable_stream( rs.stream.depth, self.width, self.height, rs.format.any, self.fps )
+
+        self.sensor = pipeline_profile = self.pipe.start( self.cfg  )
+
+        self.getIntrinsics()
 
     def closeConnection(self):
-        pass
+        self.pipe.stop()
+
+    def getIntrinsics( self ):
+        profile = self.pipe.get_active_profile()
+        profile = profile.get_stream( rs.stream.depth )
+
+        self.intrin = profile.as_video_stream_profile().get_intrinsics()
+
+        self.scale = self.sensor.get_depth_scale()
+
+        self.FOV = rs.rs2_fov( self.intrin )
+        self.FOV = np.deg2rad( self.FOV )
 
     def getFrame(self):
-        pass
+        frames = self.pipe.wait_for_frames()
+
+        # Get depth data
+        depth = frames.get_depth_frame()
+        if not depth:
+            return None
+
+        depth_points = np.asarray( depth.get_data(), np.float32 )
+
+        # Convert to meters
+        depth_points *= self.scale
+
+        return depth_pointss
--- modules/realsense/test_d435.py
+++ modules/realsense/test_d435.py
@@ -1,3 +1,9 @@
 import d435
+import numpy as np
 
 t265Obj = d435.rs_t265()
+
+with t265Obj:
+    for i in range(10):
+        frame = t265Obj.getFrame()
+        print(frame[240, 320])
