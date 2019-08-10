718a08b6152709cc831a1daca313769d1aac9094
Freddie Sherratt <fsherratt@gmail.com>
2019-08-10 16:27:53 +0100

    D435 is now streaming data

--- modules/realsense/d435.py
+++ modules/realsense/d435.py
@@ -9,8 +9,19 @@ class unexpectedDisconnect( Exception):
     pass
 
 class rs_t265:
-    def __init__(self):
-        pass
+    def __init__(self, width=640, height=480, framerate=6):
+        self.width = width
+        self.height = height
+
+        self.framerate = framerate
+
+        self.intrin = None
+        self.scale = 1
+
+        self._FOV = (0, 0)
+
+        self.xDeprojectMatrix = None
+        self.yDeprojectMatrix = None
 
     def __enter__(self):
         self.openConnection()
@@ -20,7 +31,11 @@ class rs_t265:
             print(traceback.tb_frame)
 
         self.closeConnection()
-
+    
+    # --------------------------------------------------------------------------
+    # openConnection
+    # return void
+    # --------------------------------------------------------------------------
     def openConnection(self):
         self.pipe = rs.pipeline()
 
@@ -29,26 +44,56 @@ class rs_t265:
         self.fps = 6
 
         self.cfg = rs.config()
-        self.cfg.enable_stream( rs.stream.depth, self.width, self.height, rs.format.any, self.fps )
+        self.cfg.enable_stream( rs.stream.depth, self.width, self.height, \
+                                rs.format.any, self.framerate )
 
-        self.sensor = pipeline_profile = self.pipe.start( self.cfg  )
+        self.profile = self.pipe.start( self.cfg  )
 
-        self.getIntrinsics()
-
+        self.initialise_deprojection_matrix()
+    
+    # --------------------------------------------------------------------------
+    # closeConnection
+    # return void
+    # --------------------------------------------------------------------------
     def closeConnection(self):
         self.pipe.stop()
 
+    # --------------------------------------------------------------------------
+    # getIntrinsics
+    # get camera intrinsics
+    # return void
+    # --------------------------------------------------------------------------
     def getIntrinsics( self ):
         profile = self.pipe.get_active_profile()
-        profile = profile.get_stream( rs.stream.depth )
 
-        self.intrin = profile.as_video_stream_profile().get_intrinsics()
-
-        self.scale = self.sensor.get_depth_scale()
+        self.intrin = profile.get_stream( rs.stream.depth ).as_video_stream_profile().get_intrinsics()
+        self.scale = profile.get_device().first_depth_sensor().get_depth_scale()
 
         self.FOV = rs.rs2_fov( self.intrin )
         self.FOV = np.deg2rad( self.FOV )
 
+    # --------------------------------------------------------------------------
+    # initialise_deprojection_matrix
+    # Conversion matrix from detpth to
+    # return void
+    # --------------------------------------------------------------------------
+    def initialise_deprojection_matrix( self ):
+        self.getIntrinsics()
+        
+        # Create deproject row/column vector
+        self.xDeprojectRow = (np.arange( self.width ) - self.intrin.ppx) / self.intrin.fx
+        self.yDeprojectCol = (np.arange( self.height ) - self.intrin.ppy) / self.intrin.fy
+
+        # Tile across full matrix height/width
+        self.xDeprojectMatrix = np.tile( self.xDeprojectRow, (self.height, 1) )
+        self.yDeprojectMatrix = np.tile( self.yDeprojectCol, (self.width, 1) ).transpose()
+
+
+    # --------------------------------------------------------------------------
+    # getFrame
+    # Retrieve a depth frame with scale metres from camera
+    # return np.float32[width, height]
+    # --------------------------------------------------------------------------
     def getFrame(self):
         frames = self.pipe.wait_for_frames()
 
@@ -59,7 +104,16 @@ class rs_t265:
 
         depth_points = np.asarray( depth.get_data(), np.float32 )
 
-        # Convert to meters
-        depth_points *= self.scale
+        return depth_points
 
-        return depth_pointss
+    # --------------------------------------------------------------------------
+    # deproject_frame
+    # Conversion depth frame to 3D local coordiate system in meters
+    # return [[x,y,z]] coordinates of depth pixels
+    # --------------------------------------------------------------------------
+    def deproject_frame( self, frame ):
+        Z = frame * self.scale
+        X = np.multiply( frame, xDeprojectMatrix )
+        Y = np.multiply( frame, yDeprojectMatrix )
+
+        return np.asanyarray( [X, Y, Z] )
--- modules/realsense/test_d435.py
+++ modules/realsense/test_d435.py
@@ -1,9 +1,17 @@
 import d435
 import numpy as np
+import cv2
 
-t265Obj = d435.rs_t265()
+d435Obj = d435.rs_t265( framerate = 30 )
 
-with t265Obj:
-    for i in range(10):
-        frame = t265Obj.getFrame()
-        print(frame[240, 320])
+with d435Obj:
+    for i in range(1000):
+        frame = d435Obj.getFrame()
+        
+        frame = frame * (d435Obj.scale*25)
+        frame = np.asarray(frame, np.uint8)
+        
+        frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
+
+        cv2.imshow('frame', frame)
+        cv2.waitKey(1)
