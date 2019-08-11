2304bcadc7b4cbe54fcda0d76cced7a839f22946
Freddie Sherratt <fsherratt@gmail.com>
2019-08-11 09:42:25 +0100

    Split loop and map update functions

--- modules/map.py
+++ modules/map.py
@@ -1,18 +1,9 @@
-from realsense.t265 import rs_t265
-from realsense.d435 import rs_d435
 import numpy as np
-
-import time
-
 from scipy import interpolate
-
 import cv2
 
 class mapper:
     def __init__(self):
-        self._t265 = rs_t265()
-        self._d435 = rs_d435( framerate = 30 )
-
         xRange = [-10, 10]
         yRange = [-10, 10]
         zRange = [-3, 3]
@@ -32,37 +23,30 @@ class mapper:
                                                                bounds_error = False,
                                                                fill_value = np.nan )
 
-    def loop(self):
-        with self._t265, self._d435:
-            while True:
-                t0 = time.time()
-                # Get frames of data - points and global 6dof
-                depthFrame = self._d435.getFrame()
-                pos, r, _ = self._t265.getFrame()
+    def update(self, frame, pos, r):
+        # Filter depth frame so that some parts are ignored
+        outOfRange = np.where( (frame > 1) | (frame < 0.5) )
+        frame[outOfRange] = np.nan
 
-                # Filter depth frame so that some parts are ignored
-                outOfRange = np.where( (depthFrame > 1) | (depthFrame < 0.5) )
-                depthFrame[outOfRange] = np.nan
+        # Convert to list of 3D coordinates
+        frame = self._d435.deproject_frame( frame )
+        points = np.reshape(frame, (3, -1)).transpose()
+        
+        # Filter out invalid points
+        points = points[ ~np.isnan(points[:, 2]), :]
 
-                # Convert to list of 3D coordinates
-                depthFrame = self._d435.deproject_frame( depthFrame )
-                points = np.reshape(depthFrame, (3, -1)).transpose()
-                
-                # Filter out invalid points
-                points = points[ ~np.isnan(points[:, 2]), :]
+        # Transform into global coordinate frame
+        points_global = r.apply( points )
+        points_global += np.tile(pos, (points.shape[0], 1))
 
-                # Transform into global coordinate frame
-                points_global = r.apply( points )
-                points_global += np.tile(pos, (points.shape[0], 1))
+        # Update map
+        xSort = np.digitize( points_global[:, 0], self.xBins ) - 1
+        ySort = np.digitize( points_global[:, 1], self.yBins ) - 1
+        zSort = np.digitize( points_global[:, 2], self.zBins ) - 1
 
-                # Update map
-                xSort = np.digitize( points_global[:, 0], self.xBins ) - 1
-                ySort = np.digitize( points_global[:, 1], self.yBins ) - 1
-                zSort = np.digitize( points_global[:, 2], self.zBins ) - 1
+        np.add.at(self.grid, [xSort, ySort, zSort], 1)
 
-                np.add.at(self.grid, [xSort, ySort, zSort], 1)
-
-                # Top down view
-                tmpGrid = np.sum( self.grid, axis=2 )
-                cv2.imshow('grid', tmpGrid)
-                cv2.waitKey(1)
+        # Top down view
+        tmpGrid = np.sum( self.grid, axis=2 )
+        cv2.imshow('grid', tmpGrid)
+        cv2.waitKey(1)
--- modules/test_mapping.py
+++ modules/test_mapping.py
@@ -1,9 +1,21 @@
+from realsense.t265 import rs_t265
+from realsense.d435 import rs_d435
+
 import map
+
+t265 = rs_t265()
+d435 = rs_d435( framerate = 30 )
 
 mapObj = map.mapper()
 
 try:
-    mapObj.loop()
+    with t265, d435:
+        while True:
+            # Get frames of data - points and global 6dof
+            frame = d435.getFrame()
+            pos, r, _ = t265.getFrame() 
+
+            mapObj.update()
 
 except KeyboardInterrupt:
     pass
