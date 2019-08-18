a2024119adda2bb918fd72c3dabdc149374f17ee
Freddie Sherratt <fsherratt@gmail.com>
2019-08-18 14:56:57 +0100

    Started adding mode setting and heartbeat
    Stupid firewalls got in the way... changing computer

--- modules/pixhawk.py
+++ modules/pixhawk.py
@@ -16,6 +16,8 @@ class pixhawkAbstract(mavThread.mavThread, object):
         self._attitude = [0,0,0]
         self._position = [0,0,0]
 
+        self.mode_mapping = {0: 'UNKNOWN'}
+
         super( pixhawkAbstract, self).__init__( conn, mavLib )
 
     # Process Messages
@@ -53,8 +55,8 @@ class pixhawkAbstract(mavThread.mavThread, object):
                 self._armed = False
             
             # Mode
-            mode_mapping = mavutil.mode_mapping_bynumber(msg.type)
-            self._mode = mode_mapping[msg.custom_mode]
+            self._mode = mavutil.mode_string_v10(msg)
+            self.mode_mapping = mavutil.mode_mapping_byname(msg.type)
 
     def _systemTimeHandler(self, msg):
         self._pixhawkTimeOffset = msg.time_unix_usec - time.time()*1e6
@@ -101,14 +103,30 @@ class pixhawkAbstract(mavThread.mavThread, object):
         pass
         # return self._position - self._home
 
-    # Send messages
-    def setMode(self, mode):
-        # Only subset required - land, takeoff, guided
-        msg = self._mavLib.MAVLink_command_long_message(0, 0, self._mavLib.MAV_CMD_DO_SET_MODE, 0, 0,0,0,0,0,0,0)
+    def sendHeartbeat(self):
+        msg = pymavlink.MAVLink_heartbeat_message(pymavlink.MAV_TYPE_ONBOARD_CONTROLLER, pymavlink.MAV_AUTOPILOT_INVALID, 0,0, pymavlink.MAV_STATE_ACTIVE,2)
         self.queueOutputMsg(msg)
 
-    def setHome(self):
-        use_current = 1 # 1 use current location, 0 use specificified location
+    # Send messages - Not implemented
+    def setModeGuided(self):
+        self._setMode('GUIDED')
+
+    def setModeLand(self):
+        self._setMode('LAND')
+
+    def _setMode(self, custom_mode):
+        # Only subset required - land, takeoff, guided
+        mode = pymavlink.MAV_MODE_GUIDED_ARMED
+        custom_mode = self.mode_mapping['GUIDED']
+        msg = self._mavLib.MAVLink_command_long_message(1,0,self._mavLib.MAV_CMD_DO_SET_MODE,0,mode,custom_mode,0,0,0,0,0)
+        self.queueOutputMsg(msg)
+
+    def takeoff(self, alt):
+            msg = pymavlink.MAVLink_command_long_message(0, 0, pymavlink.MAV_CMD_NAV_TAKEOFF, 0,0,0,0,0,0,0, alt)
+            self.queueOutputMsg(msg)
+
+    def setHome(self, use_current = 1):
+        # 1 use current location, 0 use specificified location
         msg = self._mavLib.MAVLink_command_long_message( 0, 0, self._mavLib.MAV_CMD_DO_SET_HOME, 0, use_current, 0,0,0,0,0,0)
         self.queueOutputMsg(msg)
 
@@ -116,22 +134,30 @@ class pixhawkAbstract(mavThread.mavThread, object):
         msg = self._mavLib.MAVLink_command_long_message(0,0, self._mavLib.MAV_CMD_GET_HOME_POSITION, 0,0,0,0,0,0,0,0)
         self.queueOutputMsg(msg)
     
-    def sendPosition(self, pos, conf, loopClosure):
-        covar = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # Covariance matrix start with NaN if not knowm
-        reset = 0 # Increment on loopclosure - not sure this can currently be extracted from realsense
-
+    def sendPosition(self, pos):
         msg = self._mavLib.MAVLink_vision_position_estimate_message(self.UNIX_time, pos[0], pos[1], pos[2], 
-                                                                pos[3], pos[4], pos[5], covar, reset)
+                                                                pos[3], pos[4], pos[5])
         self.queueOutputMsg( msg, priority=1) # Highest priority
+
+    def sendConditionYaw(self, heading, rate=25, dir=1, offset=0):
+        # heading in degrees
+        # yaw rate in deg/s
+        # dir 1 CW, -1 CCW
+        # offset 1 relative, 0 absolute
+
+        msg = self._mavLib.MAVLink_command_long_message(0, 0, self._mavLib.MAV_CMD_CONDITION_YAW, 0, 
+                                                        heading, rate, dir, offset, 0,0,0)
+        self.queueOutputMsg(msg)
 
     def requestHighLatency(self):
         msg = self._mavLib.MAVLink_command_long_message(0, 0, self._mavLib.MAV_CMD_CONTROL_HIGH_LATENCY, 0, 1, 0,0,0,0,0,0)
         self.queueOutputMsg(msg)
 
+
 if __name__ == "__main__":
     # Connect to pixhawk
-    commObj = mavSocket.mavSocket( broadcastPort = 14552, 
-                                    listenPort = 14553 )
+    commObj = mavSocket.mavSocket( broadcastPort = 14550,
+                                    listenPort = 14551 )
     
     mavObj = pixhawkAbstract( conn = commObj, 
                               mavLib = pymavlink )
@@ -142,8 +168,15 @@ if __name__ == "__main__":
     pixThread.start()
 
     try:
+        time.sleep(5)
+        mavObj.setModeGuided()
+        print('Set Mode Guided')
+        time.sleep(1)
+        mavObj.takeoff(5)
+
         while True:
-            time.sleep(1)
+            mavObj.sendHeartbeat()
+            time.sleep(0.5)
 
     except KeyboardInterrupt:
         pass
